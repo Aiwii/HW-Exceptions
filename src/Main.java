d4b2569a80c3ed651a9246291a9a8feda25d2776
Aiwii <abakar10.08@mail.ru>
2025-01-27 17:05:43 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,21 @@
+import exception.WrongLoginException;
+import exception.WrongPasswordException;
+
 public class Main {
+    public static void main(String[] args) {
+
+        String login = "java_skypro_go";
+        String password = "D_1hWiKjjP_9";
+        String confirmPassword = "D_1hWiKjjP_9";
+
+        try {
+            Validator.validate(login, password, confirmPassword);
+        } catch (WrongLoginException wrongLoginException) {
+            System.out.println("Произошла ошибка при проверке логина: " + wrongLoginException.getMessage());
+        } catch (WrongPasswordException wrongPasswordException) {
+            System.out.println("Произошла ошибка при проверке пароля: " + wrongPasswordException.getMessage());
+        } finally {
+            System.out.println("Валидация завершена!");
+        }
+    }
+}
